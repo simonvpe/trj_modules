45e63750b78a6a16521e83f0721c995f15dfd698
Simon Pettersson <simon.v.pettersson@gmail.com>
2015-09-30 10:42:00 +0200

    bla

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,11 @@
 import time
 
 def run(args):
-    while True:
+    i = 0
+    while i < 5:
         print "Hello world %s" % str(args)
         time.sleep(1)
+        i = i + 1
 
 if __name__ == "__main__":
     run(None)
