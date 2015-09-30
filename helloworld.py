fa795803fa17fc9fedd01132481eb3cc0a0161b3
Simon Pettersson <simon.v.pettersson@gmail.com>
2015-09-30 13:29:32 +0200

    update

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,14 @@
 import time
 
 def run(args):
-    i = 0
-    while i < 5:
+    ret = 0
+    while ret < 5:
         print "Hello world %s" % str(args)
         time.sleep(1)
-        i = i + 1
+        ret = ret + 1
+    return ret
 
 if __name__ == "__main__":
-    run(None)
+    print run(None)
 
 # TEST AGAIN
